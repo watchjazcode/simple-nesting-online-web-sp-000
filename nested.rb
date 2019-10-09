54b6c03c10be76728ae42ac9dc34da5ecf333a4a
Jazmin M <jazminmerckel@gmail.com>
2019-10-09 16:03:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
      }
 programmer_hash[:yukihiro_matsumoto] = { :known_for => "Ruby", :languages => ["LISP", "C"] }
     
-return programmer_hash
+return programmer_hash #return changed hash
 end
 
 def changing_alan
@@ -107,7 +107,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+programmer_hash[:alan_kay][:known_for => "GUI", :languages => []}
 end
 
 def adding_to_dennis
